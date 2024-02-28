43f9717eddf85acef5b5b42fd71f6561bc308333
Thiago <thiago2b9s@gmail.com>
2024-02-27 21:13:15 -0300

    feat: config de api e func de signout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, FormEvent } from "react";
+import { useContext, FormEvent, useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from '../styles/home.module.scss'
@@ -15,12 +15,17 @@ import Link from "next/link";
 export default function Home() {
   const {signIn} = useContext(AuthContext)
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [loading, setLoading] = useState(false);
+
   async function handleLogin(event: FormEvent){
     event.preventDefault();
 
     let data = {
-      email: "teste@teste.com",
-      password: "123123"
+      email,
+      password
     }
 
     await signIn(data)
@@ -33,18 +38,22 @@ export default function Home() {
       </Head>
 
       <div className={styles.containerCenter}>
-        <Image src={logoImg} alt="logo Sujeito Pizzaria"/>
+        <Image priority={true} src={logoImg} alt="logo Sujeito Pizzaria"/>
 
         <div className={styles.login}>
           <form onSubmit={handleLogin}>
             <Input
               placeholder="Digite seu email"
               type="text"
+              value={email}
+              onChange={ (e) => setEmail(e.target.value)}
             />
 
             <Input
               placeholder="Digite sua senha"
               type="password"
+              value={password}
+              onChange={ (e) => setPassword(e.target.value)}
             />
 
             <Button
