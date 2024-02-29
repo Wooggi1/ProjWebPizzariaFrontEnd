d0f5f7ea0c1801343bb5c17c6d050ff13d1adc82
Thiago <thiago2b9s@gmail.com>
2024-02-29 04:14:20 -0300

    feat: dashboard feito (sem modal)

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,34 @@
 import { canSSRAuth } from "@/utils/canSSRAuth"
 import Head from "next/head"
-import { Header } from "@/components/Header"
+import styles from './styles.module.scss'
+import { useState } from "react"
 
-export default function Dashboard(){
+import { Header } from "@/components/Header"
+import { FiRefreshCcw } from "react-icons/fi"
+
+import { setUpAPIClient } from "@/services/api"
+
+type OrderItem = {
+  id: string;
+  table: string | number;
+  status: boolean;
+  draft: boolean;
+  name: string | null;
+}
+
+interface HomeProps{
+  orders: OrderItem[];
+}
+
+
+export default function Dashboard({ orders }: HomeProps){
+
+  const [orderList, setOrderList] = useState(orders || []);
+
+  function handleOpenModalView(id: string){
+    alert("ID CLICADO " + id)
+  }
+
   return (
     <>
       <Head>
@@ -10,15 +36,42 @@ export default function Dashboard(){
       </Head>
       <div>
         <Header/>
-        <h1>Painel</h1>
+        <main className={styles.container}>
+
+          <div className={styles.containerHeader}>
+            <h1>Ultimos pedidos</h1>
+            <button>
+              <FiRefreshCcw size={25} color="#3fffa3"/>
+            </button>
+          </div>
+
+          <article className={styles.listOrders}>
+
+            {orderList.map( item => (
+              <section key={item.id} className={styles.orderItem}>
+                <button onClick={ () => handleOpenModalView(item.id)}>
+                  <div className={styles.tag}></div>
+                  <span>Mesa {item.table}</span>
+                </button>
+              </section>
+            ))}
+
+          </article>
+
+        </main>
       </div>
     </>
   )
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
+  const apiClient = setUpAPIClient(ctx);
+
+  const response = await apiClient.get('/orders');
 
   return {
-    props: {}
+    props: {
+      orders: response.data
+    }
   }
 })
